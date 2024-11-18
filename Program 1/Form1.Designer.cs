a2f42aedb3c6fbf2f430ab983446b38bca5f4a64
glody <glody@LAPTOP-59J886HT>
2024-11-18 15:06:36 -0500

    Add project files.

---
+++ Program 1/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Program_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtClientName = new System.Windows.Forms.TextBox();
+            this.txtCarType = new System.Windows.Forms.TextBox();
+            this.txtWorkHours = new System.Windows.Forms.TextBox();
+            this.txtPartsCost = new System.Windows.Forms.TextBox();
+            this.txtStaffAssigned = new System.Windows.Forms.TextBox();
+            this.txtSeniorCitizen = new System.Windows.Forms.TextBox();
+            this.getQuoteBtn = new System.Windows.Forms.Button();
+            this.lblTotalCost = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(388, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to Smiths Auto Repair!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Client Name:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 139);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Car Type:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(39, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Work Hours:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(39, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Parts Cost:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(39, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(118, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Staff Assigned:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(39, 292);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(174, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Senior Citizen? (0 or 1):";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(39, 399);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Total Cost:";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // txtClientName
+            // 
+            this.txtClientName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtClientName.Location = new System.Drawing.Point(256, 99);
+            this.txtClientName.Name = "txtClientName";
+            this.txtClientName.Size = new System.Drawing.Size(144, 26);
+            this.txtClientName.TabIndex = 8;
+            this.txtClientName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtCarType
+            // 
+            this.txtCarType.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCarType.Location = new System.Drawing.Point(256, 139);
+            this.txtCarType.Name = "txtCarType";
+            this.txtCarType.Size = new System.Drawing.Size(144, 26);
+            this.txtCarType.TabIndex = 9;
+            // 
+            // txtWorkHours
+            // 
+            this.txtWorkHours.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWorkHours.Location = new System.Drawing.Point(256, 176);
+            this.txtWorkHours.Name = "txtWorkHours";
+            this.txtWorkHours.Size = new System.Drawing.Size(144, 26);
+            this.txtWorkHours.TabIndex = 10;
+            this.txtWorkHours.TextChanged += new System.EventHandler(this.txtWorkHours_TextChanged);
+            // 
+            // txtPartsCost
+            // 
+            this.txtPartsCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPartsCost.Location = new System.Drawing.Point(256, 212);
+            this.txtPartsCost.Name = "txtPartsCost";
+            this.txtPartsCost.Size = new System.Drawing.Size(144, 26);
+            this.txtPartsCost.TabIndex = 11;
+            this.txtPartsCost.TextChanged += new System.EventHandler(this.txtPartsCost_TextChanged);
+            // 
+            // txtStaffAssigned
+            // 
+            this.txtStaffAssigned.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStaffAssigned.Location = new System.Drawing.Point(256, 251);
+            this.txtStaffAssigned.Name = "txtStaffAssigned";
+            this.txtStaffAssigned.Size = new System.Drawing.Size(144, 26);
+            this.txtStaffAssigned.TabIndex = 12;
+            this.txtStaffAssigned.TextChanged += new System.EventHandler(this.txtStaffAssigned_TextChanged);
+            // 
+            // txtSeniorCitizen
+            // 
+            this.txtSeniorCitizen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSeniorCitizen.Location = new System.Drawing.Point(256, 289);
+            this.txtSeniorCitizen.Name = "txtSeniorCitizen";
+            this.txtSeniorCitizen.Size = new System.Drawing.Size(144, 26);
+            this.txtSeniorCitizen.TabIndex = 13;
+            // 
+            // getQuoteBtn
+            // 
+            this.getQuoteBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.getQuoteBtn.Location = new System.Drawing.Point(102, 339);
+            this.getQuoteBtn.Name = "getQuoteBtn";
+            this.getQuoteBtn.Size = new System.Drawing.Size(187, 37);
+            this.getQuoteBtn.TabIndex = 14;
+            this.getQuoteBtn.Text = "Get Quote!";
+            this.getQuoteBtn.UseVisualStyleBackColor = true;
+            this.getQuoteBtn.Click += new System.EventHandler(this.getQuoteBtn_Click);
+            // 
+            // lblTotalCost
+            // 
+            this.lblTotalCost.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblTotalCost.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalCost.Location = new System.Drawing.Point(171, 399);
+            this.lblTotalCost.Name = "lblTotalCost";
+            this.lblTotalCost.Size = new System.Drawing.Size(118, 23);
+            this.lblTotalCost.TabIndex = 15;
+            this.lblTotalCost.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.getQuoteBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(433, 459);
+            this.Controls.Add(this.lblTotalCost);
+            this.Controls.Add(this.getQuoteBtn);
+            this.Controls.Add(this.txtSeniorCitizen);
+            this.Controls.Add(this.txtStaffAssigned);
+            this.Controls.Add(this.txtPartsCost);
+            this.Controls.Add(this.txtWorkHours);
+            this.Controls.Add(this.txtCarType);
+            this.Controls.Add(this.txtClientName);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Quote Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtClientName;
+        private System.Windows.Forms.TextBox txtCarType;
+        private System.Windows.Forms.TextBox txtWorkHours;
+        private System.Windows.Forms.TextBox txtPartsCost;
+        private System.Windows.Forms.TextBox txtStaffAssigned;
+        private System.Windows.Forms.TextBox txtSeniorCitizen;
+        private System.Windows.Forms.Button getQuoteBtn;
+        private System.Windows.Forms.Label lblTotalCost;
+    }
+}
+
---
+++ Program 1/Form1.cs
@@ -0,0 +1,139 @@
+﻿// Program 1
+// CIS 199-01
+// Due Date: 9/26/2024
+// By: Glodi Mbongo
+
+/*This Program serve as Price Quote Calculator designed for Smith's Auto Repairs. The program collects the client’s name, car type, work hours,
+parts cost, staff assigned, and senior citizen status (via a TextBox where the user enters 1 for Yes or 0 for No).
+It then calculates the total repair cost, applying a discount if the client is a senior citizen, and displays the result.
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void getQuoteBtn_Click(object sender, EventArgs e)
+        {
+            // Declare constants for the fixed values
+            const double consultationFee = 100;  //Fixed consultation fee
+            const double laborChargeHour = 75;  //Fixed labor charge per hour
+            const double seniorDiscount = 0.10; // 105 Discount for senior citizens
+
+            // declare variable
+            int workHours;   // Variable of work Work Hours
+            double partsCost;   // Variable of Parts Cost
+            int StaffAssigned;    // Variable of Staff Assigned 
+            int isSeniorCitizen;   // Variable of Senior Citizen
+            double totalCost;      // Variables total Cost
+
+            // Get inputs from the text boxes
+            // Collecting the client's name and car type as strings
+            string clientName = txtClientName.Text;  // Client's name
+            string carType = txtCarType.Text;        // Type of car 
+
+            // Parsing numerical inputs for work hours, parts cost, and staff assigned
+            workHours = int.Parse(txtWorkHours.Text);  // Number of hours of work
+            partsCost = double.Parse(txtPartsCost.Text);   // Cost of the parts used
+            StaffAssigned = int.Parse(txtStaffAssigned.Text);   // Number of staff working on the job
+
+
+            isSeniorCitizen = int.Parse(txtSeniorCitizen.Text); // Senior citizen status is entered as 1 (Yes) or 0 (No) in the text box
+
+            // Calculate total cost before applying any discount
+            totalCost = consultationFee + (laborChargeHour * workHours * StaffAssigned) + partsCost;
+
+            // Apply the senior citizen discount if the client is a senior citizen
+            if (isSeniorCitizen == 1)
+            {
+                // Subtract 10% from total if applicable
+                totalCost -= totalCost * seniorDiscount;
+            }
+            // Display the calculated total cost in the label, formatted to 2 decimal places
+            lblTotalCost.Text = $"${totalCost:F2}";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtWorkHours_TextChanged(object sender, EventArgs e)
+        {
+            // This methode handles errors when the user typethe wrong format in the Work Hour textbox
+            try
+            {
+                int workHours = int.Parse(txtWorkHours.Text);
+            }
+            catch
+            {
+                // Message when the user types the wrong format and clear the input field
+                MessageBox.Show("Error: Please Enter a valid number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtWorkHours.Text = "";
+            }
+        }
+
+        private void txtPartsCost_TextChanged(object sender, EventArgs e)
+        {
+            // This methode handles errors when the user typethe wrong format in the Part Cost textbox
+            try
+            {
+                int partsCost = int.Parse(txtPartsCost.Text);
+            }
+            catch
+            {
+                // Message when the user types the wrong format and clear the input field
+                MessageBox.Show("Error: Please Enter a valid number of Cost.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtPartsCost.Text = "";
+            }
+        }
+
+        private void txtStaffAssigned_TextChanged(object sender, EventArgs e)
+        {
+            {
+                // This methode handles errors when the user typethe wrong format in the Staff Assigned textbox
+                try
+                {
+                    int staffAssigned = int.Parse(txtStaffAssigned.Text);
+                }
+                catch
+                {
+                    // Message when the user types the wrong format and clear the input field
+                    MessageBox.Show("Error: Please Enter a valid number of Staff member assigned to the job .", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtStaffAssigned.Text = "";
+                }
+            }
+        }
+
+    }
+}
